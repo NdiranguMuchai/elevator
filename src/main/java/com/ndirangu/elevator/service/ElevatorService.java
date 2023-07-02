9bc689e478e79ae0bb6a2d1e407091994cf9dbc3
NdiranguMuchai <ndirangu.muchai@gmail.com>
2023-07-02 14:22:49 +0300

    logic to move elevator

--- src/main/java/com/ndirangu/elevator/service/ElevatorService.java
+++ src/main/java/com/ndirangu/elevator/service/ElevatorService.java
@@ -3,5 +3,5 @@ package com.ndirangu.elevator.service;
 import com.ndirangu.elevator.model.Elevator;
 
 public interface ElevatorService {
-    Elevator move();
+    Elevator move(Elevator elevator,Long requestedFloor,  Long destination);
 }
--- src/main/java/com/ndirangu/elevator/service/impl/ElevatorServiceImpl.java
+++ src/main/java/com/ndirangu/elevator/service/impl/ElevatorServiceImpl.java
@@ -1,25 +1,105 @@
 package com.ndirangu.elevator.service.impl;
 
 import com.ndirangu.elevator.model.Elevator;
+import com.ndirangu.elevator.repository.ElevatorRepository;
 import com.ndirangu.elevator.service.ElevatorService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Service
+@Slf4j
+@AllArgsConstructor
 public class ElevatorServiceImpl implements ElevatorService {
-
+private final ElevatorRepository elevatorRepository;
+    //can it handle more than 1 person requesting
+    // at what point should we save to db
+    // more logging?
     @Override
-    public Elevator move() {
+    public Elevator move(Elevator elevator,Long requestedFloor,  Long destination) {
 
-        /*
-        if at ground floor/lowest floor cant move down
-        if at highest floor cant move up
+        long distance = requestedFloor - elevator.getFloor();
 
-        add elevator object to method signature
-        change isMoving to true
-        increase or decrease value of floor every 5 secs
-        add logging here?
+        if (distance == 0){
+         openDoors(elevator.getName());
+        }
+        else {
+            //move to requested floor & open doors
+            Long start = elevator.getFloor();
+            elevator.setIsMoving(true);
+            startCounting(start, distance, elevator, requestedFloor);
+            openDoors(elevator.getName());
+        }
 
-         */
-        return null;
+        //move to destination
+        Long distanceToDestination = destination - requestedFloor;
+        elevator.setIsMoving(true);
+        startCounting(requestedFloor, distanceToDestination, elevator, destination);
+        openDoors(elevator.getName());
+
+        return elevatorRepository.save(elevator);
+    }
+
+
+    //todo: improve impl
+    private void openDoors(String name){
+
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                log.info("Elevator {} Doors Open", name);
+            }
+        };
+        timer.schedule(task, 0, 1000);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                timer.cancel();
+                log.info("Elevator {} Doors Closed", name);
+            }
+        }, 2000);
+    }
+
+
+    //todo: improve impl
+    private void startCounting(Long start, Long distance, Elevator elevator, Long destination) {
+
+        Timer timer = new Timer();
+        String direction;
+
+        if (distance > 0){
+            direction = "UP";
+        }else {
+            direction = "DOWN";
+        }
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+               elevator.setFloor(start + direction(distance));
+
+               log.info("Elevator {} is currently at floor {} moving {}", elevator.getName(), elevator.getFloor(), direction);
+
+               if (Objects.equals(elevator.getFloor(), destination)){
+                   timer.cancel();
+               }
+            }
+        };
+
+        timer.schedule(task, 0, 5000);
+    }
+
+    private int direction( Long distance){
+        if (distance > 0){
+            return 1;
+        }else {
+            return -1;
+        }
     }
 }
